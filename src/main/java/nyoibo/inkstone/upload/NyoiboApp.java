7b938099db56f5df509f705f80769636ae470fb0
frankdevhub@163.com <frankdevhub@163.com>
2019-04-20 20:24:30 +0800

    init repository of nyoibo inkstone upload util

---
+++ src/main/java/nyoibo/inkstone/upload/NyoiboApp.java
@@ -0,0 +1,19 @@
+package nyoibo.inkstone.upload;
+
+/**
+ * <p>Title:App.java</p>  
+ * <p>Description: the FTP client using to upload files to google drive and inkstone platform</p>  
+ * <p>Copyright: Copyright (c) 2019</p>  
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>  
+ * @author frankdevhub   
+ * @date 2019-04-20 20:12:35
+ */
+
+public class NyoiboApp 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
