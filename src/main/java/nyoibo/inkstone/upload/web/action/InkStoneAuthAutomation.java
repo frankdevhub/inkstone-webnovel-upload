46f76e1a19303781e0695829d4df9c232adbca09
frankdevhub@163.com <frankdevhub@163.com>
2019-04-21 01:35:25 +0800

    web page automation action

---
+++ src/main/java/nyoibo/inkstone/upload/web/action/InkStoneAuthAutomation.java
@@ -0,0 +1,15 @@
+package nyoibo.inkstone.upload.web.action;
+
+/**
+ * <p>Title:InkStoneWebPageAutomation.java</p>  
+ * <p>Description: </p>  
+ * <p>Copyright: Copyright (c) 2019</p>  
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>  
+ * @author frankdevhub   
+ * @date:2019-04-21 01:34
+ */
+
+public class InkStoneAuthAutomation {
+
+}
