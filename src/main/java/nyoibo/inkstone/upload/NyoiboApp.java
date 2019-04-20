f0caedae454a7a8e8eb8b0859e54b6db88978a0a
frankdevhub@163.com <frankdevhub@163.com>
2019-04-20 23:26:33 +0800

    logging utils

--- src/main/java/nyoibo/inkstone/upload/NyoiboApp.java
+++ src/main/java/nyoibo/inkstone/upload/NyoiboApp.java
@@ -1,19 +1,21 @@
 package nyoibo.inkstone.upload;
 
+import nyoibo.inkstone.upload.data.logging.LoggerFactory;
+import nyoibo.inkstone.upload.message.MessageMethod;
+
 /**
  * <p>Title:App.java</p>  
  * <p>Description: the FTP client using to upload files to google drive and inkstone platform</p>  
  * <p>Copyright: Copyright (c) 2019</p>  
  * <p>Company: www.frankdevhub.site</p>
  * <p>github: https://github.com/frankdevhub</p>  
- * @author frankdevhub   
+ * 
+ * @author frankdevhub
  * @date 2019-04-20 20:12:35
  */
 
-public class NyoiboApp 
-{
-    public static void main( String[] args )
-    {
-        
-    }
+public class NyoiboApp {
+	public static void main(String[] args) {
+		LoggerFactory.getLogger(NyoiboApp.class).begin().headerAction(MessageMethod.EVENT).info("application start!");
+	}
 }
