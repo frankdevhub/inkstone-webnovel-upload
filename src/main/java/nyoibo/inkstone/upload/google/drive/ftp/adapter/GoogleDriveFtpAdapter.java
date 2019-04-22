0defff4cf4b4b34cafa2ef2c5dd6174679d46371
frankdevhub@163.com <frankdevhub@163.com>
2019-04-23 02:07:03 +0800

    google drive ftp adapter

---
+++ src/main/java/nyoibo/inkstone/upload/google/drive/ftp/adapter/GoogleDriveFtpAdapter.java
@@ -0,0 +1,15 @@
+package nyoibo.inkstone.upload.google.drive.ftp.adapter;
+
+/**
+ * <p>Title:GoogleDriveFtpAdapter.java</p>  
+ * <p>Description: </p>  
+ * <p>Copyright: Copyright (c) 2019</p>  
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>  
+ * @author frankdevhub   
+ * @date:2019-04-23 02:05
+ */
+
+public class GoogleDriveFtpAdapter {
+
+}
