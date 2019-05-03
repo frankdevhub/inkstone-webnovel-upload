5803b6ecb77f3f56eece40d0327cd239fe9cc077
frankdevhub@163.com <frankdevhub@163.com>
2019-05-03 19:17:22 +0800

    inkstone google drive file backup service

---
+++ src/main/java/nyoibo/inkstone/upload/google/drive/service/GoogleDriveBackupService.java
@@ -0,0 +1,15 @@
+package nyoibo.inkstone.upload.google.drive.service;
+
+/**
+ * <p>Title:GoogleDriveBackupService.java</p>  
+ * <p>Description: </p>  
+ * <p>Copyright: Copyright (c) 2019</p>  
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>  
+ * @author frankdevhub   
+ * @date:2019-05-03 19:16
+ */
+
+public class GoogleDriveBackupService {
+
+}
