66f42eba230668d952a7103e287747972e616c1d
frankdevhub@163.com <frankdevhub@163.com>
2019-05-06 16:47:31 +0800

    custom google drive backup thread

--- src/main/java/nyoibo/inkstone/upload/google/drive/service/GoogleFileDownloadTask.java
+++ src/main/java/nyoibo/inkstone/upload/google/drive/service/GoogleFileDownloadTask.java
@@ -7,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 import nyoibo.inkstone.upload.data.logging.Logger;
 import nyoibo.inkstone.upload.data.logging.LoggerFactory;
@@ -22,42 +23,52 @@ import nyoibo.inkstone.upload.message.MessageMethod;
  * @date:2019-05-05 00:26
  */
 
-public class GoogleFileDownloadTask implements Callable<Boolean> {
+public class GoogleFileDownloadTask extends FutureTask<Boolean> {
 
 	private final String localPath;
 	private final InputStream stream;
-    private Logger LOGGER = LoggerFactory.getLogger(GoogleFileDownloadTask.class);
-	
-	public GoogleFileDownloadTask (String local, InputStream stream){
-		super();
-		this.localPath = local;
-		this.stream = stream;
-	}
-	
-	@Override
-	public Boolean call() {
-		try {
-			downloadSource(localPath, stream);
-		} catch (IOException e) {
-			LOGGER.begin().headerAction(MessageMethod.ERROR).error(e.getMessage());
-			return Boolean.FALSE;
-		}
-		return Boolean.TRUE;
+	private Logger LOGGER = LoggerFactory.getLogger(GoogleFileDownloadTask.class);
+
+	public GoogleFileDownloadTask createTask(String localPath, InputStream stream) {
+
+		Callable<Boolean> service = create(localPath, stream);
+		return new GoogleFileDownloadTask(service, localPath, stream);
 	}
 
-	private void downloadSource(String localPath,InputStream stream) throws IOException{
+	private Callable<Boolean> create(String localPath, InputStream stream) {
+		return new Callable<Boolean>() {
+
+			@Override
+			public Boolean call() throws Exception {
+				try {
+					downloadSource(localPath, stream);
+				} catch (Exception e) {
+					LOGGER.begin().headerAction(MessageMethod.ERROR).error(e.getMessage());
+				}
+				return Boolean.TRUE;
+			}
+		};
+	}
+
+	private GoogleFileDownloadTask(Callable<Boolean> callable, String localPath, InputStream stream) {
+		super(callable);
+		this.localPath = localPath;
+		this.stream = stream;
+	}
+
+	private void downloadSource(String localPath, InputStream stream) throws IOException {
 		File downloadFile = new File(localPath);
 		byte[] buffer = new byte[1024];
 		FileOutputStream fos = new FileOutputStream(downloadFile);
-		
+
 		int len = 0;
 		BufferedInputStream bis = new BufferedInputStream(stream);
 		BufferedOutputStream bos = new BufferedOutputStream(fos);
-	    
-		while((len=bis.read(buffer))>0){
+
+		while ((len = bis.read(buffer)) > 0) {
 			bos.write(buffer, 0, len);
 		}
-	
+
 		bis.close();
 		bos.flush();
 		bos.close();
@@ -67,5 +78,4 @@ public class GoogleFileDownloadTask implements Callable<Boolean> {
 		return localPath;
 	}
 
-	
 }
--- src/main/java/nyoibo/inkstone/upload/google/drive/service/InkstoneCustomThreadPoolExecutor.java
+++ src/main/java/nyoibo/inkstone/upload/google/drive/service/InkstoneCustomThreadPoolExecutor.java
@@ -19,23 +19,23 @@ import org.apache.tomcat.util.threads.ThreadPoolExecutor;
 public class InkstoneCustomThreadPoolExecutor {
 	public static ExecutorService googleBackUpPool() {
 		ExecutorService service = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MICROSECONDS,
-				new LinkedBlockingQueue<Runnable>(10)) {
+				new LinkedBlockingQueue<Runnable>(10)){
+					
+					@Override
+					protected void afterExecute(Runnable r, Throwable t) {
+						super.afterExecute(r, t);
+					}
 
-			@Override
-			protected void afterExecute(Runnable r, Throwable t) {
-				super.afterExecute(r, t);
-			}
+					@Override
+					protected void beforeExecute(Thread t, Runnable r) {
+						super.beforeExecute(t, r);
+					}
 
-			@Override
-			protected void beforeExecute(Thread t, Runnable r) {
-				super.beforeExecute(t, r);
-			}
-
-			@Override
-			protected void terminated() {
-				super.terminated();
-			}
-
+					@Override
+					protected void terminated() {
+						super.terminated();
+					}
+			
 		};
 		return service;
 	}
