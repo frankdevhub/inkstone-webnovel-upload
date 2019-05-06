f4218835570a82413ce9255e56f66d9ff5fee5e6
frankdevhub@163.com <frankdevhub@163.com>
2019-05-06 22:13:50 +0800

    custom thread pool

--- src/main/java/nyoibo/inkstone/upload/google/drive/service/InkstoneCustomThreadPoolExecutor.java
+++ src/main/java/nyoibo/inkstone/upload/google/drive/service/InkstoneCustomThreadPoolExecutor.java
@@ -3,8 +3,10 @@ package nyoibo.inkstone.upload.google.drive.service;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.tomcat.util.threads.ThreadPoolExecutor;
+import nyoibo.inkstone.upload.data.logging.Logger;
+import nyoibo.inkstone.upload.data.logging.LoggerFactory;
+import nyoibo.inkstone.upload.message.MessageMethod;
 
 /**
  * <p>Title:InkstoneCustomThreadPoolExecutor.java</p>  
@@ -17,27 +19,38 @@ import org.apache.tomcat.util.threads.ThreadPoolExecutor;
  */
 
 public class InkstoneCustomThreadPoolExecutor {
+	
+   //Nthreads=Ncpu*(1+w/c)
 	public static ExecutorService googleBackUpPool() {
-		ExecutorService service = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MICROSECONDS,
-				new LinkedBlockingQueue<Runnable>(10)){
-					
-					@Override
-					protected void afterExecute(Runnable r, Throwable t) {
-						super.afterExecute(r, t);
-					}
 
-					@Override
-					protected void beforeExecute(Thread t, Runnable r) {
-						super.beforeExecute(t, r);
-					}
+		int nCPU = Runtime.getRuntime().availableProcessors();
+		ExecutorService service = new ThreadPoolExecutor(3, 2 * nCPU, 0L, TimeUnit.MICROSECONDS,
+				new LinkedBlockingQueue<Runnable>(300)) {
+			private final Logger LOGGER = LoggerFactory.getLogger(InkstoneCustomThreadPoolExecutor.class);
 
-					@Override
-					protected void terminated() {
-						super.terminated();
-					}
-			
+			@Override
+			protected void afterExecute(Runnable r, Throwable t) {
+				GoogleFileDownloadTask currentTask = (GoogleFileDownloadTask) r;
+				LOGGER.begin().headerAction(MessageMethod.EVENT)
+						.info(String.format("File download complete:[%s]", currentTask.getLocalPath()));
+				super.afterExecute(r, t);
+			}
+
+			@Override
+			protected void beforeExecute(Thread t, Runnable r) {
+				GoogleFileDownloadTask currentTask = (GoogleFileDownloadTask) r;
+				LOGGER.begin().headerAction(MessageMethod.EVENT)
+						.info(String.format("File download start:[%s]", currentTask.getLocalPath()));
+				super.beforeExecute(t, r);
+			}
+
+			@Override
+			protected void terminated() {
+				super.terminated();
+			}
+
 		};
 		return service;
 	}
-
+  
 }
