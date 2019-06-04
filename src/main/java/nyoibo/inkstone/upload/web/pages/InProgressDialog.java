69fc2c3568e76844a11d81906df7e1313a4a03c7
frankdevhub@163.com <frankdevhub@163.com>
2019-06-04 15:18:41 +0800

    inkstone gui tools update

---
+++ src/main/java/nyoibo/inkstone/upload/web/pages/InProgressDialog.java
@@ -0,0 +1,44 @@
+package nyoibo.inkstone.upload.web.pages;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class InProgressDialog extends Dialog {
+	public InProgressDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+	@Override
+	protected int getShellStyle() {
+		return super.getShellStyle() | SWT.RESIZE | SWT.MAX;
+	}
+
+	protected Button createButton(Composite parent, int id, String label, boolean defaultButton) {
+		return null;
+	}
+
+	public final int SELECT_MANNUALLY = 101;
+	public final int TERMINAT = 402;
+
+	protected void initializeBounds() {
+		Composite comp = (Composite) getButtonBar();
+		super.createButton(comp, SELECT_MANNUALLY, "Select Mannually", true);
+		super.createButton(comp, TERMINAT, "Terminate", false);
+		super.initializeBounds();
+	}
+
+	protected Point getInitialSize() {
+		return new Point(300, 400);
+	}
+
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		InProgressDialog a = new InProgressDialog(shell);
+	}
+}
--- src/main/java/nyoibo/inkstone/upload/web/pages/InkstoneChapterPage.java
+++ src/main/java/nyoibo/inkstone/upload/web/pages/InkstoneChapterPage.java
@@ -4,7 +4,11 @@ import java.io.File;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.MessageBox;
@@ -347,8 +351,33 @@ public class InkstoneChapterPage implements Runnable {
 	 * System.out.println(a.split("—")[1]); }
 	 */
 
-	public static void main(String[] args) {
+	class InProgressDialog extends Dialog {
+		public InProgressDialog(Shell parentShell) {
+			super(parentShell);
+		}
+
+		@Override
+		protected int getShellStyle() {
+			return super.getShellStyle() | SWT.RESIZE | SWT.MAX;
+		}
+
+		protected Button createButton(Composite parent, int id, String label, boolean defaultButton) {
+			return null;
+		}
+
+		public final int SELECT_MANNUALLY = 101;
+		public final int TERMINAT = 402;
+
+		protected void initializeBounds() {
+			Composite comp = (Composite) getButtonBar();
+			super.createButton(comp, SELECT_MANNUALLY, "Select Mannually", true);
+			super.createButton(comp, TERMINAT, "Terminate", false);
+			super.initializeBounds();
+		}
+
+		protected Point getInitialSize() {
+			return new Point(300, 400);
+		}
 
 	}
-
 }
