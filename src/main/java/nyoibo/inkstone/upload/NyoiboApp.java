14aa8ebdc9c714abe566a04ed640fcc79402777e
frankdevhub@163.com <frankdevhub@163.com>
2019-06-07 16:55:03 +0800

    prepare packege entrance

--- src/main/java/nyoibo/inkstone/upload/NyoiboApp.java
+++ src/main/java/nyoibo/inkstone/upload/NyoiboApp.java
@@ -8,22 +8,22 @@ import org.springframework.context.annotation.ComponentScan;
 import nyoibo.inkstone.upload.gui.InkstoneUploadMainWindow;
 
 /**
- * <p>Title:App.java</p>  
- * <p>Description: the FTP client using to upload files to google drive and inkstone platform</p>  
- * <p>Copyright: Copyright (c) 2019</p>  
+ * <p>Title:App.java</p>
+ * <p>Description: the FTP client using to upload files to google drive and inkstone platform</p>
+ * <p>Copyright: Copyright (c) 2019</p>
  * <p>Company: www.frankdevhub.site</p>
- * <p>github: https://github.com/frankdevhub</p>  
- * 
+ * <p>github: https://github.com/frankdevhub</p>
+ *
  * @author frankdevhub
  * @date 2019-04-20 20:12:35
  */
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
-@ComponentScan(basePackages = { "nyoibo.inkstone.upload" })
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = {"nyoibo.inkstone.upload"})
 public class NyoiboApp {
-	public static void main(String[] args) throws Exception {
-		InkstoneUploadMainWindow main = new InkstoneUploadMainWindow(Display.getDefault().getActiveShell());
-		main.open();
-	}
+    public static void main(String[] args) throws Exception {
+        InkstoneUploadMainWindow main = new InkstoneUploadMainWindow(Display.getDefault().getActiveShell());
+        main.open();
+    }
 }
