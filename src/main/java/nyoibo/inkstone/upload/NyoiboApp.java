74dce5dac5920bf637e6acd390699781fbefbc63
frankdevhub@163.com <frankdevhub@163.com>
2019-06-07 18:14:24 +0800

    prepare packege entrance

--- src/main/java/nyoibo/inkstone/upload/NyoiboApp.java
+++ src/main/java/nyoibo/inkstone/upload/NyoiboApp.java
@@ -5,13 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+
 import nyoibo.inkstone.upload.gui.InkstoneUploadMainWindow;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan(basePackages = { "nyoibo.inkstone.upload" })
-public class NyoiboApp {
+public class NyoiboApp extends SpringBootServletInitializer {
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(NyoiboApp.class, args);
 		InkstoneUploadMainWindow main = new InkstoneUploadMainWindow(Display.getDefault().getActiveShell());
